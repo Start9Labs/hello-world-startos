cf0d396e4c8d2415bd9025005283e8a2c042625c
Lucy C <12953208+elvece@users.noreply.github.com>
2022-03-01 00:01:54 -0700

    Fix/updates (#14)
    * fix backups
    
    * update health check with success message
    
    * adjust brevity in health check syntax
    
    * simplify manifest with defaults and add examples with other options

--- check-web.sh
+++ check-web.sh
@@ -4,9 +4,7 @@ DURATION=$(</dev/stdin)
 if (($DURATION <= 5000)); then
     exit 60
 else
-    curl --silent --fail hello-world.embassy &>/dev/null
-    RES=$?
-    if test "$RES" != 0; then
+    if ! curl --silent --fail hello-world.embassy &>/dev/null; then
         echo "Web interface is unreachable" >&2
         exit 1
     fi
