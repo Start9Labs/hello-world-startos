8c30c5af980db6602f09f9fb09790d2bac90619c
BluJ <mogulslayer@gmail.com>
2023-04-25 10:33:51 -0600

    chore: Update the version

--- startos/procedures/autoConfig.ts
+++ startos/procedures/autoConfig.ts
@@ -1,10 +1,16 @@
 import { setupAutoConfig } from 'start-sdk/lib/autoconfig'
 import { ConfigSpec } from './config/spec'
 import { WrapperData } from '../wrapperData'
+import { Manifest } from '../manifest'
 
 /**
  * In this function, you establish rules for auto configuring service dependencies
- * 
+ *
  * See "Hello Moon" for an example
  */
-export const autoConfig = setupAutoConfig<WrapperData, ConfigSpec>({})
+export const autoConfig = setupAutoConfig<
+  WrapperData,
+  ConfigSpec,
+  Manifest,
+  {}
+>({})
