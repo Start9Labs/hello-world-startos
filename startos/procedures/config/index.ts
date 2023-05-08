e7d21d4dae22aa01cce13b97b1691d79c3047df5
BluJ <mogulslayer@gmail.com>
2023-05-08 12:42:45 -0600

    feat: Update version

--- startos/procedures/config/index.ts
+++ startos/procedures/config/index.ts
@@ -2,14 +2,16 @@ import { WrapperData, wrapperDataContract } from '../../wrapperData'
 import { configSpec } from './spec'
 import { read } from './read'
 import { save } from './save'
-import { Manifest } from '../../manifest'
+import { Manifest, manifest } from '../../manifest'
 import setupConfig from '@start9labs/start-sdk/lib/config/setupConfig'
 
 /**
  * This is a static file. There is no need to make changes here
  */
-export const { getConfig, setConfig } = setupConfig<
-  WrapperData,
-  typeof configSpec,
-  Manifest
->(wrapperDataContract, configSpec, save, read)
+export const { getConfig, setConfig } = setupConfig(
+  wrapperDataContract,
+  manifest,
+  configSpec,
+  save,
+  read,
+)
