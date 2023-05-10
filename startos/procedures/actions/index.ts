c7ecd6f4a0c6e2300bab21d71a5f725dd3de2b3f
Matt Hill <matthewonthemoon@gmail.com>
2023-05-09 18:02:07 -0600

    minor bug fix and commnets

--- startos/procedures/actions/index.ts
+++ startos/procedures/actions/index.ts
@@ -1,8 +1,8 @@
 import { sdk } from '../sdk'
 import { nameToLogs } from './nameToLogs'
-import { setupActions } from '@start9labs/start-sdk/lib/actions/setupActions'
 
 /**
  * Add each new Action as the next argument to this function
  */
+
 export const { actions, actionsMetadata } = sdk.setupActions(nameToLogs)
--- startos/procedures/sdk.ts
+++ startos/procedures/sdk.ts
@@ -3,6 +3,11 @@ import { manifest } from '../manifest'
 import { Store } from '../store'
 import { Vault } from '../vault'
 
+/**
+ * This is a static file that provides type safety throughout the codebase
+ *
+ * the exported sdk const should be used instead of StartSdk directly
+ */
 export const sdk = StartSdk.of()
   .withManifest(manifest)
   .withStore<Store>()
