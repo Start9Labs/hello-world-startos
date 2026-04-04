de8c115b8d625dac54bd26e8d1e33d342229e3f3
Matt Hill <mattnine@protonmail.com>
2026-04-04 15:32:15 -0600

    040

--- startos/versions/v2.0.0.2.ts
+++ startos/versions/v2.0.0.2.ts
@@ -3,11 +3,11 @@ import { VersionInfo } from '@start9labs/start-sdk'
 export const v_2_0_0_2 = VersionInfo.of({
   version: '2.0.0:2',
   releaseNotes: {
-    en_US: 'Update to StartOS SDK beta.59',
-    es_ES: 'Actualización a StartOS SDK beta.59',
-    de_DE: 'Update auf StartOS SDK beta.59',
-    pl_PL: 'Aktualizacja do StartOS SDK beta.59',
-    fr_FR: 'Mise à jour vers StartOS SDK beta.59',
+    en_US: 'Initial release for StartOS 0.4.0',
+    es_ES: 'Lanzamiento inicial para StartOS 0.4.0',
+    de_DE: 'Erstveröffentlichung für StartOS 0.4.0',
+    pl_PL: 'Pierwsze wydanie dla StartOS 0.4.0',
+    fr_FR: 'Version initiale pour StartOS 0.4.0',
   },
   migrations: {
     up: async ({ effects }) => {},
