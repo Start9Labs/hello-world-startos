10c8302275070bee71e243d10cb2e3e035a479c0
Matt Hill <mattnine@protonmail.com>
2026-05-02 09:10:05 -0600

    file naming convention

--- startos/versions/index.ts
+++ startos/versions/index.ts
@@ -1,5 +1,5 @@
 import { VersionGraph } from '@start9labs/start-sdk'
-import { v_2_0_0_4 } from './v2.0.0.4'
+import { v_2_0_0_4 } from './v2.0.0_4'
 
 export const versionGraph = VersionGraph.of({
   current: v_2_0_0_4,
